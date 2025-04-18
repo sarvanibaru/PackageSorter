758cc84d5fb0476c38de184d880c9621a671afa0
Satya Sarvani Baru <sarvanibaru@gmail.com>
2025-04-18 15:05:55 -0700

    Create sortPackage.py

---
+++ sortPackage.py
@@ -0,0 +1,12 @@
+def sort(width, height, length, mass):
+    volume = width * height * length
+
+    is_bulky = volume >= 1_000_000 or width >= 150 or height >= 150 or length >= 150
+    is_heavy = mass >= 20
+
+    if is_bulky and is_heavy:
+        return "REJECTED"
+    elif is_bulky or is_heavy:
+        return "SPECIAL"
+    else:
+        return "STANDARD"
